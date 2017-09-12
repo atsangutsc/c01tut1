9cf9a61e28399c99d61cdcd581277e4b6d96a1d0
Abraham Ho Ming Tsang <tsangabr@IITS-B473-04.utsc-labs.utoronto.ca>
2017-09-12 11:21:49 -0400

    made output more descriptive

--- factorial.py
+++ factorial.py
@@ -1,9 +1,9 @@
 def factorial(n):
   if n == 1:
-    return  1
+    return  n
   else:
     return n * factorial(n - 1)
 
 if __name__ == "__main__":
   for i in range(1, 11):
-    print factorial(i)
+    print "the factorial of %s is %s" % (i,  factorial(i))
