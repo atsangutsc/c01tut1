75bbed16abd8b61fe9792fc0b6397e781edee422
James Gabriel Goudie <goudiej1@IITS-B473-03.utsc-labs.utoronto.ca>
2017-09-12 11:25:21 -0400

    Modified the final output and change the implementation of factorial.

--- factorial.py
+++ factorial.py
@@ -1,9 +1,11 @@
 def factorial(n):
   if n == 1:
-    return  1
+    return  0
   else:
     return n * factorial(n - 1)
 
 if __name__ == "__main__":
   for i in range(1, 11):
     print factorial(i)
+
+  print("I don't know what I'm doing.")
